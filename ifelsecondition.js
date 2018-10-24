09ac418f4c51bd5b02ad77accaa18c53649938d1
rakesh315 <41191136+rakesh315@users.noreply.github.com>
2018-10-24 12:05:42 +0530

    Add files via upload

---
+++ ifelsecondition.js
@@ -0,0 +1,13 @@
+<script>
+var d = new Date();
+var time = d.getHours();
+if (time<10){
+document.write("Good Morning");
+}
+else if (time>10 && time<16){
+document.write("Good Day");
+}
+else{
+document.write(""Hello World");
+}
+</script>
